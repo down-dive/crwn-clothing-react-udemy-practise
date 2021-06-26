2bd6f142b75bfb273cd8dd7ef220194661799408
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-25 22:25:57 -0700

    item collection component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,8 +10,7 @@ const CollectionItem = () => (
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
-
-
         </div>
     </div>
-)
+);
+export default CollectionItem;
--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,13 +1,16 @@
 import './collection-preview.component.styles.scss';
 
+import './collection-preview.style.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preiew'>
         <h1 className='title'>{title.toUpperCase}</h1>
         <div className='preview'>
             {items
             .filter((  item, idx) => idx < 4)
-            .map(item => (
-                <div key={item.id}>{item.name}</div>
+            .map(({ id, ...otheritemProps})=> (
+                <CollectionItem key={id}{ ...otheritemProps} />
             ))}
         </div>
     </div>
