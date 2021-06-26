a3fdf86228fc55827580cd7b4741828969d8fe15
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-25 21:54:51 -0700

    preview collection component

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -4,9 +4,11 @@ const CollectionPreview = ({ title, items }) => (
     <div className='collection-preiew'>
         <h1 className='title'>{title.toUpperCase}</h1>
         <div className='preview'>
-
-
+            {items.map(item => (
+                <div key={item.id}>{item.name}</div>
+            ))}
         </div>
-
     </div>
-)
+);
+
+export default CollectionPreview;
