67d99ba18310eb86f1c3705b45a09d8510f39070
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-30 23:31:21 -0700

    sign up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -6,7 +6,7 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
 import './sign-up.styles.scss';
 
 class SignUp extends React.Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
@@ -17,7 +17,22 @@ class SignUp extends React.Component {
         }
     }
     render() {
-        
+        return (
+            <div className='sign-up'>
+                <h2 className='title'>I do not have an account</h2>
+                <span>Sign up with your email and password</span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type="text"
+                        name='displayName'
+                        value={displayName}
+                        label='Display Name'
+                        required
+                    >
+                    </FormInput>
+                </form>
+            </div>
+        )
     }
 }
 
